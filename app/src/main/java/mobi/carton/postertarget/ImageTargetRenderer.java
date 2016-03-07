356e4dc4dc61ce1edb6318cbbd2c4bc3e2d04b1d
Damien Brun <makowildcat@gmail.com>
2016-03-07 12:37:59 -0500

    Clean ImageTargetRenderer

--- app/src/main/java/mobi/carton/postertarget/ImageTargetRenderer.java
+++ app/src/main/java/mobi/carton/postertarget/ImageTargetRenderer.java
@@ -1,58 +1,37 @@
-/*===============================================================================
-Copyright (c) 2012-2014 Qualcomm Connected Experiences, Inc. All Rights Reserved.
-
-Vuforia is a trademark of PTC Inc., registered in the United States and other
-countries.
-===============================================================================*/
-
 package mobi.carton.postertarget;
 
 
-import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
 import android.util.Log;
 
-import com.qualcomm.vuforia.Matrix44F;
 import com.qualcomm.vuforia.Renderer;
 import com.qualcomm.vuforia.State;
-import com.qualcomm.vuforia.Tool;
 import com.qualcomm.vuforia.Trackable;
 import com.qualcomm.vuforia.TrackableResult;
-import com.qualcomm.vuforia.VIDEO_BACKGROUND_REFLECTION;
-import com.qualcomm.vuforia.Vuforia;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+
 // The renderer class for the ImageTargets sample.
-public class ImageTargetRenderer implements GLSurfaceView.Renderer {
+public class ImageTargetRenderer
+        implements
+        GLSurfaceView.Renderer {
+
+
     private static final String LOGTAG = "ImageTargetRenderer";
+
 
     private SampleApplicationSession vuforiaAppSession;
     private ImageTargets mActivity;
 
-    private int shaderProgramID;
-
-    private int vertexHandle;
-
-    private int normalHandle;
-
-    private int textureCoordHandle;
-
-    private int mvpMatrixHandle;
-
-
     private Renderer mRenderer;
 
-    boolean mIsActive = false;
 
-    private static final float OBJECT_SCALE_FLOAT = 3.0f;
+    public boolean mIsActive;
 
 
-    public ImageTargetRenderer(ImageTargets activity,
-                               SampleApplicationSession session)
-    {
+    public ImageTargetRenderer(ImageTargets activity, SampleApplicationSession session) {
         mActivity = activity;
         vuforiaAppSession = session;
     }
@@ -60,8 +39,7 @@ public class ImageTargetRenderer implements GLSurfaceView.Renderer {
 
     // Called to draw the current frame.
     @Override
-    public void onDrawFrame(GL10 gl)
-    {
+    public void onDrawFrame(GL10 gl) {
         if (!mIsActive)
             return;
 
@@ -72,11 +50,13 @@ public class ImageTargetRenderer implements GLSurfaceView.Renderer {
 
     // Called when the surface is created or recreated.
     @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config)
-    {
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         Log.d(LOGTAG, "GLRenderer.onSurfaceCreated");
 
-        initRendering();
+        // Function for initializing the renderer.
+        mRenderer = Renderer.getInstance();
+        // Hide the Loading Dialog
+        mActivity.loadingDialogHandler.sendEmptyMessage(LoadingDialogHandler.HIDE_LOADING_DIALOG);
 
         // Call Vuforia function to (re)initialize rendering after first use
         // or after OpenGL ES context was lost (e.g. after onPause/onResume):
@@ -86,8 +66,7 @@ public class ImageTargetRenderer implements GLSurfaceView.Renderer {
 
     // Called when the surface changed size.
     @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height)
-    {
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
         Log.d(LOGTAG, "GLRenderer.onSurfaceChanged");
 
         // Call Vuforia function to handle render surface size changes:
@@ -95,108 +74,20 @@ public class ImageTargetRenderer implements GLSurfaceView.Renderer {
     }
 
 
-    // Function for initializing the renderer.
-    private void initRendering() {
-
-        mRenderer = Renderer.getInstance();
-
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, Vuforia.requiresAlpha() ? 0.0f
-                : 1.0f);
-
-        shaderProgramID = SampleUtils.createProgramFromShaderSrc(
-                CubeShaders.CUBE_MESH_VERTEX_SHADER,
-                CubeShaders.CUBE_MESH_FRAGMENT_SHADER);
-
-        vertexHandle = GLES20.glGetAttribLocation(shaderProgramID,
-                "vertexPosition");
-        normalHandle = GLES20.glGetAttribLocation(shaderProgramID,
-                "vertexNormal");
-        textureCoordHandle = GLES20.glGetAttribLocation(shaderProgramID,
-                "vertexTexCoord");
-        mvpMatrixHandle = GLES20.glGetUniformLocation(shaderProgramID,
-                "modelViewProjectionMatrix");
-
-
-        // Hide the Loading Dialog
-        mActivity.loadingDialogHandler
-                .sendEmptyMessage(LoadingDialogHandler.HIDE_LOADING_DIALOG);
-    }
-
-
     // The render function.
-    private void renderFrame()
-    {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
+    private void renderFrame() {
         State state = mRenderer.begin();
         mRenderer.drawVideoBackground();
 
-        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
-
-        // handle face culling, we need to detect if we are using reflection
-        // to determine the direction of the culling
-        GLES20.glEnable(GLES20.GL_CULL_FACE);
-        GLES20.glCullFace(GLES20.GL_BACK);
-        if (Renderer.getInstance().getVideoBackgroundConfig().getReflection() == VIDEO_BACKGROUND_REFLECTION.VIDEO_BACKGROUND_REFLECTION_ON)
-            GLES20.glFrontFace(GLES20.GL_CW); // Front camera
-        else
-            GLES20.glFrontFace(GLES20.GL_CCW); // Back camera
-
         // did we find any trackables this frame?
-        for (int tIdx = 0; tIdx < state.getNumTrackableResults(); tIdx++)
-        {
+        for (int tIdx = 0; tIdx < state.getNumTrackableResults(); tIdx++) {
             TrackableResult result = state.getTrackableResult(tIdx);
             Trackable trackable = result.getTrackable();
-            printUserData(trackable);
-            Matrix44F modelViewMatrix_Vuforia = Tool
-                    .convertPose2GLMatrix(result.getPose());
-            float[] modelViewMatrix = modelViewMatrix_Vuforia.getData();
 
-            // deal with the modelview and projection matrices
-            float[] modelViewProjection = new float[16];
-
-            if (!mActivity.isExtendedTrackingActive())
-            {
-                Matrix.translateM(modelViewMatrix, 0, 0.0f, 0.0f,
-                        OBJECT_SCALE_FLOAT);
-                Matrix.scaleM(modelViewMatrix, 0, OBJECT_SCALE_FLOAT,
-                        OBJECT_SCALE_FLOAT, OBJECT_SCALE_FLOAT);
-            }
-
-            Matrix.multiplyMM(modelViewProjection, 0, vuforiaAppSession
-                    .getProjectionMatrix().getData(), 0, modelViewMatrix, 0);
-
-            // activate the shader program and bind the vertex/normal/tex coords
-            GLES20.glUseProgram(shaderProgramID);
-
-            if (!mActivity.isExtendedTrackingActive())
-            {
-                GLES20.glEnableVertexAttribArray(vertexHandle);
-                GLES20.glEnableVertexAttribArray(normalHandle);
-                GLES20.glEnableVertexAttribArray(textureCoordHandle);
-
-                // pass the model view matrix to the shader
-                GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false,
-                        modelViewProjection, 0);
-
-                // disable the enabled arrays
-                GLES20.glDisableVertexAttribArray(vertexHandle);
-                GLES20.glDisableVertexAttribArray(normalHandle);
-                GLES20.glDisableVertexAttribArray(textureCoordHandle);
-            }
-
-            SampleUtils.checkGLError("Render Frame");
-
+            String userData = (String) trackable.getUserData();
+            Log.d(LOGTAG, "UserData:Retreived User Data	\"" + userData + "\"");
         }
 
-        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-
         mRenderer.end();
-    }
-
-
-    private void printUserData(Trackable trackable) {
-        String userData = (String) trackable.getUserData();
-        Log.d(LOGTAG, "UserData:Retreived User Data	\"" + userData + "\"");
     }
 }
