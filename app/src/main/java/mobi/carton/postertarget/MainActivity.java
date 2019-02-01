ee58959edd40706d47f90c8306e43aaee9ad31a4
Damien Brun <makowildcat@gmail.com>
2019-02-01 16:40:05 -0500

    Seems (sorry) to set brightness to maximum and width, height and margin to respectively 60, 35 and 25 mm

--- app/src/main/java/mobi/carton/postertarget/MainActivity.java
+++ app/src/main/java/mobi/carton/postertarget/MainActivity.java
@@ -12,11 +12,16 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -123,6 +128,34 @@ public class MainActivity extends CartonActivity
             startActivity(intentLauncher);
             finish();
         }
+
+        Window window = getWindow();
+
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION           // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN                // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+
+        // set brightness to maximum
+        layoutParams.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL;
+
+        // set width to 60 mm and height to 35 mm
+        layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, 60, dm);
+        layoutParams.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, 35, dm);
+
+        // set 10 mm margin from TOP/LEFT
+        layoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+        layoutParams.x = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, 25, dm);
+        layoutParams.y = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, 25, dm);
+
+        window.setAttributes(layoutParams);
     }
 
 
